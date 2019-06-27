972f85c68d026ce89345e1473033969e32ee9425
Olivier benson-smith <olliebenson@hotmail.co.uk>
2019-06-27 16:24:25 +0100

    tests and functionality written for jobs with and without dependencies, returning in the right order. Tests passing

--- orderedJobs.js
+++ orderedJobs.js
@@ -9,13 +9,19 @@ const orderedJobs = (jobs) => {
   let nonDependantJobsArray = [];
 
   for (let i = 0; i < jobArray.length; i++) {
-    if (jobArray[i].length > 1) {
+    if (jobArray[i].length === 1) {
+      nonDependantJobsArray.push(jobArray[i])
+    }
+    else if (jobArray[i].length > 1) {
       let nonDependantJob = jobArray[i][0]
       let dependantJob = jobArray[i][1]
       if (dependantJob === nonDependantJob) throw Error('Jobs can`t depend on themselve`s');
+
+      dependantJobsArray.push(dependantJob, nonDependantJob)
     }
   }
-  return jobArray
+  let orderedJobsArray = nonDependantJobsArray.concat(dependantJobsArray)
+  return orderedJobsArray.filter(function (item, pos) { return orderedJobsArray.indexOf(item) == pos })
 }
 
 module.exports = { orderedJobs };
--- spec/orderedJobs.spec.js
+++ spec/orderedJobs.spec.js
@@ -6,19 +6,32 @@ describe('orderedJobs', () => {
     const actual = orderedJobs('');
     const expected = [];
     expect(actual).to.eql(expected)
+    expect(expected.length).to.equal(0);
   })
   it('Returns a single array when one job is passed with no dependancies are passed', () => {
     const actual = orderedJobs('a =>');
     const expected = ['a'];
     expect(actual).to.eql(expected)
+    expect(expected.length).to.equal(1);
   });
   it('Returns an array in the correct order when multiple jobs are passed with no dependencies', () => {
     const actual = orderedJobs('a =>, b => , c =>');
     const expected = ['a', 'b', 'c'];
     expect(actual).to.eql(expected)
+    expect(expected.length).to.equal(3);
   });
   it('Returns error message `Error: Jobs can`t depend on themselve`s` if it depends on itself', () => {
     const actual = 'a => a';
     expect(function () { orderedJobs(actual) }).throw();
+  });
+  it('Returns an array in the correct order if one job has dependencies', () => {
+    const actual = orderedJobs('a =>, b => c, c => ');
+    const expected = ['a', 'c', 'b'];
+    expect(actual).to.eql(expected)
+    expect(expected.length).to.equal(3);
+    expect(actual.indexOf('a')).to.not.equal(-1);
+    expect(actual.indexOf('b')).to.not.equal(-1);
+    expect(actual.indexOf('c')).to.not.equal(-1);
+    expect(actual.indexOf('c')).to.be.lessThan(actual.indexOf('b'));
   });
 });
